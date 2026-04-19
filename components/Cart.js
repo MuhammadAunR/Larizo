236280170bd3cef84a7f02a97fcbe8e2f73e947c
M Aun Rasheed <greataun786@gmail.com>
2026-04-19 23:02:55 +0500

    fix; fixed cart and global.css

--- components/Cart.js
+++ components/Cart.js
@@ -11,10 +11,10 @@ const Cart = () => {
     return (
         <main className='w-full flex'>
 
-            <section onClick={toggleCart} className={`bg-surface/70 backdrop-blur-lg h-screen fixed top-0 z-200 w-[calc(100%-400px)] ${isCartOpen ? 'block' : 'hidden'}`}>
+            <section onClick={toggleCart} className={`bg-surface/50 backdrop-blur-lg h-screen fixed top-0 z-200 w-[calc(100%-400px)] ${isCartOpen ? 'block' : 'hidden'}`}>
             </section>
 
-            <aside className={`bg-surface h-screen w-100 fixed top-0 right-0 z-200 transition-all ease-linear ${isCartOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <aside style={{ backgroundColor: 'var(--surface)' }} className={`h-screen w-100 fixed top-0 right-0 z-200 transition-all ease-linear ${isCartOpen ? 'translate-x-0' : 'translate-x-full'}`}>
 
                 <div className='flex items-center justify-between px-5 py-6.5 border-b-2 border-accent'>
                     <h3 className='text-2xl'>Your Cart</h3>
