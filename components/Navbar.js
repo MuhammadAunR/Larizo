a035420e4b4ab947591e4b30e5cef4554e4bcb56
M Aun Rasheed <greataun786@gmail.com>
2026-04-21 17:12:51 +0500

    fix: navbar dropdown Link to a tag

--- components/Navbar.js
+++ components/Navbar.js
@@ -81,7 +81,15 @@ const Navbar = () => {
                 <ul className='flex flex-col items-center justify-center gap-3 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'>
                     {navOptions.map(opt => {
                         return <li key={opt.path} className='relative group'>
-                            <Link href={opt.path} onClick={handleNavbarDropdown} className='uppercase text-surface transition-colors ease-linear cursor-pointer'>{opt.option}</Link>
+                            <motion.a
+                                initial={{ opacity: 0, y: 30 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.6 }}
+                                viewport={{ once: true }}
+                                href={opt.path}
+                                onClick={handleNavbarDropdown}
+                                className='uppercase text-surface transition-colors ease-linear cursor-pointer'>{opt.option}
+                            </motion.a>
                             <span className='absolute h-px w-0 group-hover:w-full bg-surface transition-all ease-linear left-0 bottom-0'></span>
                         </li>
                     })}
