24549aafd10c836dcccf9e195c3a4478c2d3e32f
M Aun Rasheed <greataun786@gmail.com>
2026-04-24 06:52:00 +0500

    fix: navbar dropdown text size

--- components/NavbarDropdown.js
+++ components/NavbarDropdown.js
@@ -43,7 +43,7 @@ const NavbarDropdown = () => {
                     <Link href={'/'} className='font-serif font-bold text-5xl cursor-pointer text-foreground'>Larizo</Link>
                 </div>
                 <div className='flex items-center gap-4'>
-                    <li onClick={toggleCart} className='hover:text-accent transition-colors ease-linear cursor-pointer relative'>
+                    <li onClick={toggleCart} className='hover:text-accent  transition-colors ease-linear cursor-pointer relative'>
                         <ShoppingBag size={22} />
                         {cartItems.length > 0 &&
                             <span className='absolute w-5 h-5 rounded-full text-xs -top-2 -left-2 bg-accent flex items-center justify-center text-black'>{cartItems.length}</span>
@@ -58,7 +58,7 @@ const NavbarDropdown = () => {
             <ul className='flex flex-col items-center justify-center gap-3 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'>
                 {navOptions.map(opt => (
                     <li key={opt.path} className='relative group'>
-                        <a href={opt.path} onClick={toggleNavbar} className='uppercase text-foreground hover:text-accent transition-colors ease-linear cursor-pointer'>{opt.option}</a>
+                        <a href={opt.path} onClick={toggleNavbar} className='uppercase text-foreground hover:text-accent transition-colors ease-linear cursor-pointer text-xl md:text-2xl'>{opt.option}</a>
                         <span className='absolute h-px w-0 group-hover:w-full bg-accent transition-all ease-linear left-0 bottom-0'></span>
                     </li>
                 ))}
